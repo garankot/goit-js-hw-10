ebad83f149b0c55dc0a7cf9d459cd980174e94a6
Taras Garan'ko <garanko.t@gmail.com>
2021-11-29 14:53:19 +0200

    fix

--- dist/src.34c95dfd.js
+++ dist/src.34c95dfd.js
@@ -1266,7 +1266,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '65314' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49508' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
